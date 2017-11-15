0abcb5b31f50365c8e021b9e160fea635035eb9d
Gabriel Rodrigues Couto <gabrielrcouto@gmail.com>
2017-11-15 13:37:55 -0200

    Added content-type header

--- jsonrpc.php
+++ jsonrpc.php
@@ -4,7 +4,7 @@ require __DIR__.'/../../app/common.php';
 
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
-header("Access-Control-Allow-Headers: X-Requested-With, authorization");
+header("Access-Control-Allow-Headers: X-Requested-With, authorization, content-type");
 
 
 echo $container['api']->execute();
