31d059aa5170687bc6d75bce6e5d9194a832f362
Gabriel Rodrigues Couto <gabrielrcouto@gmail.com>
2017-11-15 13:18:25 -0200

    Added authorization header

--- jsonrpc.php
+++ jsonrpc.php
@@ -4,6 +4,7 @@ require __DIR__.'/../../app/common.php';
 
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
-header("Access-Control-Allow-Headers: X-Requested-With");
+header("Access-Control-Allow-Headers: X-Requested-With, authorization");
+
 
 echo $container['api']->execute();
